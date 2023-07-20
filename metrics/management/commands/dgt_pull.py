ea1afa05e64e0071bd9042ded2aa2894b974df7d
josericardopenase <josericardopenase@gmail.com>
2023-07-20 19:27:16 +0100

    Created dgt command

--- metrics/management/commands/dgt_pull.py
+++ metrics/management/commands/dgt_pull.py
@@ -1,11 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
-from polls.models import Question as Poll
-
 
 class Command(BaseCommand):
     help = "Pull all metrics data from dgt server"
 
     def handle(self, *args, **options):
-            self.stdout.write(
-                self.style.SUCCESS('Successfully pulled all data from dgt')
-            )
+        self.stdout.write(
+            self.style.SUCCESS('Successfully pulled all data from dgt')
+        )
