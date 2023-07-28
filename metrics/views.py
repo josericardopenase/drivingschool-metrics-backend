6d9328fe3ceff76b29a392a4917626cca25eb736
albbamartin <alba.martin112@alu.ulpgc.es>
2023-07-28 15:17:56 +0100

    Created filter metrics module

--- metrics/views.py
+++ metrics/views.py
@@ -4,7 +4,7 @@ from tests.models import Test, TestType
 from drivingschools.models import DrivingSchool, DrivingSchoolSection, DrivingPermission
 from rest_framework import serializers
 from django.db.models import Sum
-from filters.models import QueryFilter
+from metrics.filters import *
 
 @api_view()
 def graph1(request):
@@ -16,31 +16,25 @@ def graph1(request):
     autoescuelas = autoescuela_param.split(',') if autoescuela_param else []
     autoescuelas_obj = DrivingSchool.objects.filter(name__in=autoescuelas) if len(autoescuelas) > 0 else DrivingSchool.objects.all()
 
-    # parámetros permisos
-    permiso_param = request.query_params.get('permiso', "")
-    permisos = permiso_param.split(',') if permiso_param else []
-    permisos_obj = DrivingPermission.objects.filter(name__in=permisos) if len(permisos) > 0 else DrivingPermission.objects.all()
+    filters = [
+        #DateFilterField(),
+        DrivingSchoolFilterField(),
+        #MetricFilterField(), # nº aprobados, nºaptos o nºno aptos
+        PermissionFilterField(), 
+        ProvinceFilterField(),
+        TestCenterFilterField(),
+        TestTypeFilterField()]
 
-    # será el número de meses from & to 
-    year = request.query_params.get('year')
+    query_filter = QueryFilter(filters, request.query_params)
+    query_filter.validate_params()
+    filter_params = query_filter.combine_filters()
+    print(filter_params)
     
-    final_result = [None] * 12
-
-
-
-    filters = QueryFilter([ProvinceFilterField(), DrivingSchoolFilterField], request.query_params)
-    Test.objecs.filter(**filters.filter())
+    final_result = [None] * 37
 
     for autoescuela in autoescuelas_obj:
         sections = DrivingSchoolSection.objects.filter(driving_school=autoescuela)
-
-        params = {
-            'year': year,
-            'school_section__in': sections,
-            'permission_type__in': permisos_obj,
-        }
-
-        result = Test.objects.filter(params).values('month', 'year').annotate(valor=Sum(metrica_param))
+        result = Test.objects.filter(**filter_params).values('month', 'year').annotate(valor=Sum(metrica_param))
         
         for row in range(0, len(result)):
             if final_result[row] == None:
@@ -63,41 +57,29 @@ def graph1(request):
 @api_view()
 def graph2(request):
 
-    # parámetros autoescuelas
-    autoescuela_param = request.query_params.get('autoescuela', "")
-    autoescuelas = autoescuela_param.split(',') if autoescuela_param else []
-    print(len(autoescuelas))
-    autoescuelas_obj = DrivingSchool.objects.filter(name__in=autoescuelas) if len(autoescuelas) > 0 else DrivingSchool.objects.all()
-
     # parámetros permisos
     permiso_param = request.query_params.get('permiso')
     permisos = permiso_param.split(',') if permiso_param else []
-    print(permisos)
     permisos_obj = DrivingPermission.objects.filter(name__in=permisos) if len(permisos) > 0 else DrivingPermission.objects.all()
-    print(permisos_obj)
 
-    # parámetros tipo de examen
-    tipo_examen_param = request.query_params.get('tipo_examen')
-    tipo_examen = tipo_examen_param.split(',') if tipo_examen_param else []
-    print(tipo_examen)
-    tipo_examen_obj = TestType.objects.filter(name__in=tipo_examen) if len(tipo_examen) > 0 else TestType.objects.all()
-    print(tipo_examen_obj)
+    filters = [
+        #DateFilterField(),
+        DrivingSchoolFilterField(),
+        #MetricFilterField(), # nº aprobados, nºaptos o nºno aptos
+        ProvinceFilterField(),
+        TestCenterFilterField(),
+        TestTypeFilterField()]
 
-    # será el número de meses from & to 
+    query_filter = QueryFilter(filters, request.query_params)
+    query_filter.validate_params()
+    filter_params = query_filter.combine_filters()
+    print(filter_params)
 
-    # Parámetros de rango de fechas
-    year= request.query_params.get('year')  
-    month= request.query_params.get('month')  
     final_result =  []
 
     for permiso in permisos_obj:
-        sections = DrivingSchoolSection.objects.filter(driving_school__in=autoescuelas_obj)
-        result = Test.objects.filter(year=year,
-            #month=month,
-            test_type__in=tipo_examen_obj,
-            school_section__in=sections, 
-            permission_type=permiso
-        ).values('permission_type').annotate(
+        filter_params['permission_type'] = permiso
+        result = Test.objects.filter(**filter_params).values('permission_type').annotate(
         num_presentados=Sum('num_presentados'),
         num_aptos=Sum('num_aptos'))
 
@@ -116,37 +98,26 @@ def graph2(request):
 @api_view()
 def graph3(request):
 
-    # parámetros autoescuelas
-    autoescuela_param = request.query_params.get('autoescuela', '')
-    print(list(autoescuela_param))
-    autoescuelas = autoescuela_param.split(',') if autoescuela_param else []
-    print(len(autoescuelas))
-    autoescuelas_obj = DrivingSchool.objects.filter(name__in=autoescuelas) if len(autoescuelas) > 0 else DrivingSchool.objects.all()
+    filters = [
+        #DateFilterField(),
+        DrivingSchoolFilterField(),
+        #MetricFilterField(), # nº aprobados, nºaptos o nºno aptos
+        PermissionFilterField(), 
+        ProvinceFilterField(),
+        TestCenterFilterField()]
 
-    # parámetro permiso
-    permiso= request.query_params.get('permiso')  
-
-    # será el número de meses from & to 
-
-    # Parámetros de rango de fechas
-    year= request.query_params.get('year')  
-    month= request.query_params.get('month')  
-
-    print(request.query_params, " hola")
+    query_filter = QueryFilter(filters, request.query_params)
+    query_filter.validate_params()
+    filter_params = query_filter.combine_filters()
+    print(filter_params)
 
     final_result =  []
 
     for tipo_examen in TestType.objects.all():
-        sections = DrivingSchoolSection.objects.filter(driving_school__in=autoescuelas_obj)
-        result = Test.objects.filter(year=year,
-            #month=month,
-            school_section__in=sections, 
-            permission_type=permiso,
-            test_type=tipo_examen
-        ).values('test_type').annotate(
-        num_presentados=Sum('num_presentados'),
-        num_aptos=Sum('num_aptos'))
-
+        filter_params['test_type'] = tipo_examen
+        result = Test.objects.filter(**filter_params).values('test_type').annotate(
+            num_presentados=Sum('num_presentados'),
+            num_aptos=Sum('num_aptos'))
         
         for row in result:
             final_result.append({
