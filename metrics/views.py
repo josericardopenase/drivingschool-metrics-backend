07a87ce46dde718443aa353427a75d57366f0f32
josericardopenase <josericardopenase@gmail.com>
2023-08-14 02:32:06 +0100

    Fixed error

--- metrics/views.py
+++ metrics/views.py
@@ -1,4 +1,4 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from tests.models import Test, TestType
 from drivingschools.models import DrivingSchool, DrivingSchoolSection, DrivingPermission
