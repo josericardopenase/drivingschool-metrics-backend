97d82c1abeee9d06a8150ba7d1a8b64f97e0ee6b
josericardopenase <josericardopenase@gmail.com>
2024-02-16 11:30:54 +0000

    Generated new deploy to heroku

--- config/urls.py
+++ config/urls.py
@@ -30,10 +30,6 @@ urlpatterns = [
     path('tests/', include('tests.urls')),
 ]
 
-# Serve static files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
 
-# Serve media files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
