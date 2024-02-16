41809dbcdb9ea8c89b4f38588b84363a88b5d529
josericardopenase <josericardopenase@gmail.com>
2024-02-16 11:46:51 +0000

    Generated new deploy to heroku

--- tests/admin.py
+++ tests/admin.py
@@ -1,7 +1,26 @@
+from collections import Counter
+from itertools import count
 from django.contrib import admin
 from .models import Test, TestCenter, TestType
+from django.contrib import admin
+# Assuming all models are in the same app, if not adjust the import paths accordingly
 
-# Register your models here.
-admin.site.register(Test)
+
+class TestAdmin(admin.ModelAdmin):
+    list_display = ('test_center', 'test_type', 'permission_type', 'school_section', 'month', 'year', 'num_aptos', 'num_presentados', 'num_suspensos')
+    list_filter = ('test_center__province', 'school_section__driving_school', 'month', 'year', 'test_type', 'permission_type')
+    search_fields = ('test_center__name', 'test_type__name', 'permission_type__name', 'school_section__driving_school__name')
+    ordering = ('year', 'month')
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.annotate(
+            num_suspensos=Counter('num_presentados') - count('num_aptos')
+        )
+
+admin.site.register(Test, TestAdmin)
+
 admin.site.register(TestCenter)
+
+
 admin.site.register(TestType)
