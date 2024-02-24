d87e9875fc2686a2479c29270957cfb6cf5302b2
josericardopenase <josericardopenase@gmail.com>
2024-02-24 11:07:59 +0000

    Generated new deploy to heroku

--- config/settings/base.py
+++ config/settings/base.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', 'hidden-basin-07379-f12c22
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
